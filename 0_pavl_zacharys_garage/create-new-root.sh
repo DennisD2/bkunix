37cd00a0a4047b8e5ff3e0d373abe368407ecd33
DennisD2 <info@spurtikus.de>
2021-02-13 14:58:42 +0100

    On the way to have a usable root.dsk image

--- 0_pavl_zacharys_garage/create-new-root.sh
+++ 0_pavl_zacharys_garage/create-new-root.sh
@@ -4,12 +4,11 @@ FSUTIL="$BASEDIR/../fsutil/lsx-util"
 #FSUTIL="$BASEDIR/../fsutil/lsx-util -v -v -v"
 
 cd extracted-disks/root
-rm -f newroot.dsk
 
-#${FSUTIL} -n -s256000 newroot.dsk
-#${FSUTIL} -n -s256000 -bsys/boot1 -Bsys/boot2lo root.dsk
-${FSUTIL} -n -S -s256000 -b$BASEDIR/../fsutil/rxboot -B$BASEDIR/../fsutil/rxboot2 newroot.dsk
-#${FSUTIL} -n -s256000 -b"$BASEDIR/../fsutil/rxboot" newroot.dsk
+#OLD ${FSUTIL} -n -s256000 -bsys/boot1 -Bsys/boot2lo root.dsk
+
+# rm -f newroot.dsk
+#  ${FSUTIL} -n -S -s256000 -b$BASEDIR/../fsutil/rxboot -B$BASEDIR/../fsutil/rxboot2 newroot.dsk
 
 ${FSUTIL} -a newroot.dsk lsx usr/ tmp/
 ${FSUTIL} -a newroot.dsk etc/ etc/clri etc/cset etc/init etc/glob etc/mknod etc/mkfs #etc/fsck
@@ -17,7 +16,8 @@ ${FSUTIL} -a newroot.dsk bin/ bin/sh bin/ls bin/cp bin/date bin/mkdir \
     bin/mv bin/rm bin/rmdir bin/stty bin/od bin/ed  \
     bin/as bin/cc bin/check bin/db bin/ld bin/load bin/ls bin/reloc bin/sh bin/size \
     bin/strip #bin/sync bin/echo bin/cal bin/cat bin/ln bin/wc bin/pwd bin/df bin/mount bin/umount
-#${FSUTIL} -a newroot.dsk dev/ dev/tty8!c0:0 dev/fd0!b0:0 dev/fd1!b0:1
+${FSUTIL} -a newroot.dsk lib/ lib/as2  lib/c0  lib/c1  lib/c2  lib/crt0.o  lib/liba.a  lib/libc.a
+# OLD ${FSUTIL} -a newroot.dsk dev/ dev/tty8!c0:0 dev/fd0!b0:0 dev/fd1!b0:1
 ${FSUTIL} -c newroot.dsk
 ${FSUTIL} -v newroot.dsk
 
--- readme.md
+++ readme.md
@@ -290,6 +290,32 @@ u6-fsutil -c root.dsk
 u6-fsutil -n -s256000 usr.dsk
 u6-fsutil -c usr.dsk
 u6-fsutil -v root.dsk
-
 ```
 
+And for LSX Unix
+```
+# root disk
+#!/bin/bash
+BASEDIR=`pwd`
+FSUTIL="$BASEDIR/../fsutil/lsx-util"
+#FSUTIL="$BASEDIR/../fsutil/lsx-util -v -v -v"
+
+cd extracted-disks/root
+
+# rm -f newroot.dsk
+# ${FSUTIL} -n -S -s256000 -b$BASEDIR/../fsutil/rxboot -B$BASEDIR/../fsutil/rxboot2 newroot.dsk
+
+${FSUTIL} -a newroot.dsk lsx usr/ tmp/
+${FSUTIL} -a newroot.dsk etc/ etc/clri etc/cset etc/init etc/glob etc/mknod etc/mkfs #etc/fsck
+${FSUTIL} -a newroot.dsk bin/ bin/sh bin/ls bin/cp bin/date bin/mkdir \
+    bin/mv bin/rm bin/rmdir bin/stty bin/od bin/ed  \
+    bin/as bin/cc bin/check bin/db bin/ld bin/load bin/ls bin/reloc bin/sh bin/size \
+    bin/strip #bin/sync bin/echo bin/cal bin/cat bin/ln bin/wc bin/pwd bin/df bin/mount bin/umount
+${FSUTIL} -a newroot.dsk lib/ lib/as2  lib/c0  lib/c1  lib/c2  lib/crt0.o  lib/liba.a  lib/libc.a
+# OLD ${FSUTIL} -a newroot.dsk dev/ dev/tty8!c0:0 dev/fd0!b0:0 dev/fd1!b0:1
+${FSUTIL} -c newroot.dsk
+${FSUTIL} -v newroot.dsk
+
+# usr disk
+
+```
